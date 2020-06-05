c3a820d5263e84875f6190678710a6dd2d77c135
Hackmann <ichbinlange@gmail.com>
2020-06-05 19:27:41 +0200

    New Actor attribute and function calling roughly implemented

--- game.py
+++ game.py
@@ -24,61 +24,82 @@ from gamestate import gamestate
 
 class Engine:# use @classmethod so class does not need to be instantiated
     """Contains main game loop and player actions"""
+    def __init__(self):
+        self.stats = {"examine": self.examine,
+                      "use": self.use,
+                      "take": self.take,
+                      "talk to": self.talk
+                      }
+
 
     def prompt(self):
         """Prompt for user input and translation to appropriate action"""
-        self.command = input(">>> ")
+        while True:
+            self.command = input(">>> ").lower()
+            try:
+                if any((word in self.command for word in ("with", "and"))):
+                    verb, obj1, temp, obj2 = self.command.split(' ')
+                else:
+                    verb, obj1 = self.command.split(' ')
+                    obj2 = None
 
-    def take(self, obj):
+            except:
+                print("...BEEP, BOP, cannot compute...")
+            else:
+                if verb in self.stats:
+                    return verb, obj1, obj2
+                else: print("I don't know how to do that")
+        ## TODO: get words from dict
+
+    def take(self, obj1, obj2):
         """Invoke take() method of object"""
         pass
 
-    def examine(self, obj):
+    def examine(self, obj1, obj2):
         """Invoke examine() method of object"""
         pass
 
-    def talk(self, obj):
+    def talk(self, obj1, obj2):
         """Invoke talk() method of object"""
         pass
 
-    def use(self,obj):
+    def use(self, obj1, obj2):
         """Invoke use() method of object"""
+        #check if operation possible
+        if not obj1.stats.get("location") == player.stats.get("location"):
+            print(f"I don't see {obj1.stats.get('name')}")
+        elif not obj2.stats.get("location") == player.stats.get("location"):
+            print(f"I don't see {obj1.get('name')}")
+        else:
+            return obj1.use(obj2)
 
     def play(self):
         """Main game loop"""
 
         while True:
-            self.prompt()
             try:
-                if "with" in self.command:
-                    func, obj, temp, obj2 = self.command.split(' ')
-                    call = gamestate.get(obj).get("identname") + "." + func
-                           + "(obj=" + gamestate.get(obj2).get("identname") + ")"
-                else:
-                    func, obj = self.command.split(' ')
-                    obj2 = ""
-                    call = gamestate.get(obj).get("identname")+"."+func+"()"
-            except:
-                print("...beep bop, cannot compute...")
-
-            try:
-                exec(call)
-            except:
-                print("...beep bop, cannot compute...")
-            #translator function needed (lexicon)
+                verb, obj1, obj2 = self.prompt()
+                self.stats.get(verb)(gamestate.get(obj1), gamestate.get(obj2))
+            except Exception as e:
+                print(e)
+                #print("Couldn't find function or object")
 
 
 class Actor:
     """Parent Actor class. Takes keyword for user input, identifier name, location."""
 # check if **kwargs should be used.
 # Add argument to define if this actor can activate another (key and lock for example)
-    def __init__(self, keyword, identname, location, examined=False,
-                 usable=False, used=False, usetext=None):
-        self.keyword = keyword
-        gamestate[keyword] = {"identname": identname,
-                              "location": location,
-                              "usetext": usetext
-                              }
+    def __init__(self, name, location, **kwargs):
+        gamestate[name.lower()] = self
+        self.stats = {"name": name,
+                      "location": location,
+                      "examined": False,
+                      "used": False,
+                      "usable": False,
+                      "active": False,
+                      "activates": None
+                      }
+        self.stats.update(kwargs)
 
     def take(self):
         print("I can't take the idea of 'Actor'")
@@ -88,18 +109,12 @@ class Actor:
 
     def use(self, obj=None): # use decorator for checks that are alway the same
         if not obj:
-            print("Nothing happens")
-        elif gamestate.get(obj).get("location") == "Room1": #current player location
-            print("I can't combine those things")
-        else:
-            print(f"I can't find {gamestate.get(obj).get("keyword")}")
+            print("Using thing")
+        if obj in self.stats.get("activates"):
+            pass
 
     def talk(self):
         pass
-
-    def modify(self, **kwargs):
-        for kwarg in kwargs:
-            gamestate.get(self.keyword).update(kwargs)
 
 
 
@@ -107,28 +122,29 @@ class StartScreen(Actor):
     pass
 
 
-class Toaster(Actor):
-    """A Toaster"""
-    def __init__(self, name, location):
-        super().__init__(name, location)
-        gamestate[location.name][self.name] = {"usable": True}
 
 class Key(Actor):
     """A Key"""
-    def use(self, obj=None): # use decorator for checks that are alway the same
-        if not obj:
+    def use(self, obj2=None): # use decorator for checks that are alway the same
+        if not obj2:
             print("I need to use this with something.")
-        elif obj == lock:
+        elif obj2 == lock:
             print("You hear a satisfying 'CLICK'.")
-            gamestate["lock"]
+        else:
+            print("I can't combine those things")
 
 
-lock = Actor("Lock", "lock", "Room1")
-key = Actor("Key", "key", "Room1")
 
-print(gamestate.get("Actor"))
+lock = Actor("Lock", "Room1")
+key = Key("Key", "Room1")
+item = Actor("Item", "Room2")
+player = Actor("Player", "Room1")
+
+print(gamestate.items())
+
 
 engine = Engine()
+print(engine.stats)
 engine.play()
 
 ## TODO: Idea how to store all text and or game logic
