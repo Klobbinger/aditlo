15f8a1921fe05e0defbc7aacc819fdebf084366b
Hackmann <ichbinlange@gmail.com>
2020-06-08 01:55:24 +0200

    Version 0.1

--- game.py
+++ game.py
@@ -22,132 +22,139 @@ from time import sleep
     #Toaster
 #TextFile
 #SaveGame
-
-## TODO: define __getattr__ to retrieve object attribute from gamestate dict
 gamestate = {}
 
 class Engine:# use @classmethod so class does not need to be instantiated
-    """Contains main game loop and player actions"""
-    def __init__(self):
-        self.stats = {"examine": self.examine,
-                      "use": self.use,
-                      "take": self.take,
-                      "talk to": self.talk
-                      }
+    """Contains main game loop and input parser"""
 
     def clear(self):
         _ = os.system("cls") if os.name == "nt" else os.system("clear")
-
+    ## TODO: implement better parser
     def prompt(self):
-        """Prompt for user input and translation to appropriate action"""
+        """Prompt for user input and parse into verb, obj1 and obj2"""
         while True:
-            self.command = input(">>> ").lower()
+            command = input(">>> ").lower()
             self.clear()
             try:
-                if any(word in self.command for word in ("with", "and", "on")):
-                    verb, obj1, temp, obj2 = self.command.split(' ')
+                if any(word in command for word in ("with", "and", "on")):
+                    verb, obj1, temp, obj2 = command.split(' ')
                 else:
-                    verb, obj1 = self.command.split(' ')
+                    verb, obj1 = command.split(' ')
                     obj2 = None
-
             except:
                 print("...BEEP, BOP, cannot compute...")
             else:
-                if not verb in self.stats:
-                    print("I don't know how to do that")
-                elif not gamestate.get(obj1):
-                    print(f"I don't see any {obj1} here.")
-                elif not gamestate.get(obj2) and obj2:
-                    print(f"I don't see any {obj2}")
-                else:
-                    return verb, obj1, obj2
-        ## TODO: get words from dict
+                return verb, obj1, obj2
 
-    def take(self, obj1, obj2):
-        """Invoke take() method of object"""
-        if not self.interactable(obj1):
-            print(f"I don't see any {obj1.get('name')}")
-        else:
-            return obj1.take()
+# TODO: dedent correctly
+    def interact(self, verb, obj1, obj2):
+        """call interact function of object"""
+        output = ["I don't see any {}",
+        "Looking real hard, but no {} in sight.",
+        dedent("""\
+        I don't have my glasses on,
+        but I'm pretty sure there's no {} here.""")]
 
-    def examine(self, obj1, obj2):
-        """Invoke examine() method of object"""
         if not self.interactable(obj1):
-            print(f"I don't see any {obj1.get('name')}")
+            print(choice(output).format(obj1))
+        elif not self.interactable(obj2) and obj2:
+            print(choice(output).format(obj2))
         else:
-            return obj1.examine()
+            gamestate.get(obj1).interact(verb, obj2)
 
-    def talk(self, obj1, obj2):
-        """Invoke talk() method of object"""
-        if not self.interactable(obj1):
-            print(f"I don't see any {obj1.get('name')}")
-        else:
-            return obj1.talk()
 
-    def use(self, obj1, obj2):
-        """Invoke use() method of object"""
-        if not self.interactable(obj1):
-            print(f"I don't see any {obj1.get('name')}")
-        elif obj2 and not self.interactable(obj2):
-            print(f"I don't see any {obj1.get('name')}")
-        else:
-            return obj1.use(obj2)
 
     def interactable(self, obj):
-        test = obj.get("location") in (player.get("location"), "player")
-        return test
+        """check if object in player location"""
+        bool = False
+        if obj in gamestate:
+            bool = gamestate.get(obj).location in (player.location, player)
+        return bool
 
     def play(self):
         """Main game loop"""
-
         while True:
             verb, obj1, obj2 = self.prompt()
-            self.stats.get(verb)(gamestate.get(obj1), gamestate.get(obj2))
-
-                #print("Couldn't find function or object")
-
+            self.interact(verb, obj1, obj2)
 
 class Actor:
     """Parent Actor class. Takes keyword for user input, identifier name, location."""
 # check if **kwargs should be used.
 # Add argument to define if this actor can activate another (key and lock for example)
-    def __init__(self, name, location, **kwargs):
+    def __init__(self,
+                name,
+                location=None,
+                x_off="A regular {}.",
+                examined=False,
+                used=False,
+                usable=False,
+                active=False,
+                activates=[],
+                takeable=False,
+                take_false_text="I can't take that {}.",
+                take_true_text=dedent("""\
+                *{} added to inventory*
+                Maybe I can use that later."""),
+                use_words=["use", "open"],
+                examine_words=["examine", "inspect"],
+                take_words=["take", "get"],
+                talk_words=["talk", "scream"]
+                ):
+
         gamestate[name.lower()] = self
-        self.stats = {"name": name,
-                      "location": location,
-                      "examine": [f"A normal {name}", f"Just a regular {name}"],
-                      "examined": False,
-                      "used": False,
-                      "usable": False,
-                      "active": False,
-                      "activates": [],
-                      "takeable": False,
-                      "take_false_text": f"I can't take that {name}.",
-                      "take_true_text": dedent(f"""\
+        self.name = name
+        self.location = location if location else self
+        self.x_off = x_off
+        self.examined = examined
+        self.used = used
+        self.usable = usable
+        self.active = active
+        self.activates = activates
+        self.takeable = takeable
+        self.takeable_false_text = f"I can't take this {name}."
+        self.takeable_true_text = dedent(f"""\
                                         *{name} added to inventory*
                                         Maybe I can use that later.""")
-                      }
-        self.stats.update(kwargs)
+        self.use_words = ["use", "open"]
+        self.examine_words = ["examine", "inspect"]
+        self.take_words = ["take", "get"]
+        self.talk_words = ["talk", "scream"]
 
-    def get(self, key):
-        return self.stats.get(key)
+        # load command dict with accepted verbs
+        self.commands = {}
+        self.commands.update({k: self.use for k in self.use_words})
+        self.commands.update({k: self.examine for k in self.examine_words})
+        self.commands.update({k: self.take for k in self.take_words})
+        self.commands.update({k: self.use for k in self.talk_words})
 
-    def take(self):
-        if not self.stats.get("takeable"):
-            print(self.stats.get("take_false_text"))
+    def interact(self, verb, obj):
+        func = self.commands.get(verb)
+        if not func:
+            print(choice(["I can't do that.", "Nah.", "I don't know how.",
+                          "Seems dumb", "No sane person would attempt this."]))
         else:
-            print(self.stats.get("take_true_text"))
+            return func(obj)
 
-    def examine(self):
-        print(self.stats.get("examine"))
+    def take(self, obj):
+        if not self.takeable:
+            print(self.takeable_false_text.format(self.name))
+        else:
+            self.location = player
+            print(self.takeable_true_text.format(self.name))
 
-    def use(self, obj=None): # use decorator for checks that are alway the same
+    def examine(self, obj):
+        print(self.x_off.format(self.name))
+
+    def use(self, obj):
         if not obj:
-            print("Using thing")
-        if obj in self.stats.get("activates"):
+            print("Using {}".format(self.name))
+        if obj in self.activates:
             pass
 
-    def talk(self):
+    def talk(self, obj):
+        pass
+
+    def enter():
         pass
 
 
@@ -155,8 +162,8 @@ class Actor:
 class StartScreen(Actor):
 
     def enter(self):
-        if not self.stats.get("active"):
-            player.stats.update(location=self.get("location"))
+        player.location = self
+        if not self.active:
             print("...")
             sleep(3)
             print("Is this a dream?")
@@ -196,24 +203,44 @@ class Start(Actor):
         sleep(2)
         print("WELCOME TO:\n")
         sleep(2)
-        print("A DAY", flush=True)
+        print("""
+    ___       ____
+   /   |     / __ \____ ___  __
+  / /| |    / / / / __ `/ / / /
+ / ___ |   / /_/ / /_/ / /_/ /
+/_/  |_|  /_____/\__,_/\__, /
+                      /____/
+        """, flush=True)
         sleep(1)
-        print("  IN THE LIFE", flush=True)
+        print("""
+\t\t    _          __  __            __    _ ____
+\t\t   (_)___     / /_/ /_  ___     / /   (_) __/__
+\t\t  / / __ \   / __/ __ \/ _ \   / /   / / /_/ _ \\
+\t\t / / / / /  / /_/ / / /  __/  / /___/ / __/  __/
+\t\t/_/_/ /_/   \__/_/ /_/\___/  /_____/_/_/  \___/
+
+        """, flush=True)
         sleep(1)
-        print("    OF ...", flush=True)
+        print("""
+\t\t\t\t         ____
+\t\t\t\t  ____  / __/
+\t\t\t\t / __ \/ /_
+\t\t\t\t/ /_/ / __/    _ _ _
+\t\t\t\t\____/_/      (_|_|_)
+
+        """, flush=True)
         sleep(2)
         engine.clear()
         print("...ehm...")
         sleep(2)
-        player.stats.update(name=input(
-                            "...sorry kid, what's your name again?\n"))
+        player.name = input("...sorry kid, what's your name again?\n")
         sleep(1)
         print("Ah, yeah, alright whatever...")
         sleep(2)
         print("...harrumph...")
         sleep(2)
         engine.clear()
-        print("\n\n\n\t\t\tA Day in The Life of {}".format(player.get("name")),
+        print("\n\n\n\t\t\tA Day in The Life of {}".format(player.name),
               end=' ', flush=True)
         sleep(2)
         print("the LOSER!")
@@ -225,13 +252,13 @@ class Start(Actor):
         sleep(2)
         print("BRRRRRRRRRRRRRRIIIIIIIIIIIING\n")
         sleep(2)
-        return bedroom.enter()
+        #return bedroom.enter()
 
 
 
 class Key(Actor):
     """A Key"""
-    def use(self, obj2=None): # use decorator for checks that are alway the same
+    def use(self, obj2): # use decorator for checks that are alway the same
         if not obj2:
             print("I need to use this with something.")
         elif obj2 == lock:
@@ -240,19 +267,21 @@ class Key(Actor):
             print("I can't combine those things")
 
 
-lock = Actor("Lock", "Room1",
-              examine="Looks like this old lock is a little rusty.",
-              )
 
-key = Key("Key", "Room1", takeable=True)
-item = Actor("Item", "Room2")
-player = Actor("Player", "Room1")
-start_screen = StartScreen("Menu", "Menu")
-start = Start("Start", "Menu")
+bedroom = Actor("Bedroom", None)
+player = Actor("Player", bedroom)
+gamestate.update(room=player.location)
+key = Key("Key", bedroom)
+item = Actor("Item", None)
+lock = Actor("Lock", bedroom,
+            x_off="Looks like this old lock is a little rusty.")
+item2 = Actor("Item2", player)
+menu = StartScreen("Menu", None)
+start = Start("Start", menu)
 
 engine = Engine()
 engine.clear()
-start_screen.enter()
+#menu.enter()
 engine.play()
 
 ## TODO: Idea how to store all text and or game logic
